c1ad46e8c98bfded7a87c8c57a4b7472d2f6badd
Iryna Stein <iryna@sensibleweather.com>
2022-11-23 09:37:41 -0600

    initial clean up and app set up

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const App = () => {
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
+  const clickHandler = () => {
+    console.log(input);
+  };
+  return (
+    <div>
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <div>
+        <button onClick={clickHandler}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
