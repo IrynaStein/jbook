6eb4111302b71d6e34d6e654058a6852ee547039
Iryna Stein <iryna@sensibleweather.com>
2022-12-06 09:39:07 -0700

    local forage setup

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,5 +1,17 @@
 import * as esbuild from 'esbuild-wasm';
 import axios from 'axios';
+import localForage from 'localforage';
+
+const fileCache = localForage.createInstance({
+  name: 'filecache',
+});
+
+// (async () => {
+//   await fileCache.setItem('color', 'red');
+//   const color = await fileCache.getItem('color');
+//   console.log(color);
+// })();
+
 export const unpkgPathPlugin = () => {
   return {
     name: 'unpkg-path-plugin',
@@ -12,7 +24,10 @@ export const unpkgPathPlugin = () => {
         if (args.path.includes('./') || args.path.includes('../')) {
           return {
             namespace: 'a',
-            path: new URL(args.path, args.importer + '/').href,
+            path: new URL(
+              args.path,
+              'https://unpkg.com' + args.resolveDir + '/'
+            ).href,
           };
         }
         return {
@@ -28,17 +43,30 @@ export const unpkgPathPlugin = () => {
           return {
             loader: 'jsx',
             contents: `
-              const message = require('nested-test-pkg');
-              console.log(message);
+              // const react= require('react');
+              import react, {useState} from 'react-select';
+              console.log(react, useState);
             `,
           };
         }
+        //Check to see if we have already fetched this file
+        //and if it is in the cache
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
+        if (cachedResult) {
+          return cachedResult;
+        }
+        //if it is, return it immediately
+        const { data, request } = await axios.get(args.path);
+        // console.log(request);
 
-        const { data } = await axios.get(args.path);
-        return {
+        const result: esbuild.OnLoadResult = {
           loader: 'jsx',
           contents: data,
+          resolveDir: new URL('./', request.responseURL).pathname,
         };
+        //store response in cache
+        await fileCache.setItem(args.path, result);
+        return result;
       });
     },
   };
