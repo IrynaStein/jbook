dea69fe67dfebf1d538ae068ac2dfc29d6040e7e
Iryna Stein <iryna@sensibleweather.com>
2022-12-06 09:46:02 -0700

    replaced harcoded value

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -12,7 +12,7 @@ const fileCache = localForage.createInstance({
 //   console.log(color);
 // })();
 
-export const unpkgPathPlugin = () => {
+export const unpkgPathPlugin = (inputCode: string) => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
@@ -42,11 +42,7 @@ export const unpkgPathPlugin = () => {
         if (args.path === 'index.js') {
           return {
             loader: 'jsx',
-            contents: `
-              // const react= require('react');
-              import react, {useState} from 'react-select';
-              console.log(react, useState);
-            `,
+            contents: inputCode,
           };
         }
         //Check to see if we have already fetched this file
