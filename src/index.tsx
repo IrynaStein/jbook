46a2bc54579e4f60a8a8996fb7b34c35302d6a81
Iryna Stein <iryna@sensibleweather.com>
2022-12-12 18:18:02 -0700

    cleaned up App component

--- src/index.tsx
+++ src/index.tsx
@@ -1,76 +1,13 @@
-import React, { useState, useEffect, useRef } from 'react';
-import * as esbuild from 'esbuild-wasm';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
-import { fetchPlugin } from './plugins/fetch-plugin';
+import 'bulmaswatch/superhero/bulmaswatch.min.css';
+import CodeCell from './components/CodeCell';
 
 const App = () => {
-  const [input, setInput] = useState('');
-  const [code, setCode] = useState('');
-  const ref = useRef<any>();
-  const iframe = useRef<any>();
-  const startService = async () => {
-    ref.current = await esbuild.startService({
-      worker: true,
-      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
-    });
-  };
-
-  useEffect(() => {
-    startService();
-  }, []);
-
-  const clickHandler = async () => {
-    if (!ref.current) return;
-    // console.log(ref.current);
-    // const result = await ref.current.transform(input, {
-    //   loader: 'jsx',
-    //   target: 'es2015',
-    // });
-    const result = await ref.current.build({
-      entryPoints: ['index.js'],
-      bundle: true,
-      write: false,
-      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
-      define: { 'process.env.NODE_ENV': '"production"', global: 'window' },
-    });
-    console.log(result.outputFiles[0].text);
-
-    // setCode(result.outputFiles[0].text);
-    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
-  };
-
-  const html = `
-  <html>
-    <head></head>
-    <body>
-      <div id="root"></div>
-      <script>
-        window.addEventListener('message', (event) => {
-          eval(event.data)
-        }, false)
-      </script>
-    </body>
-  </html>`;
-
   return (
     <div>
-      <textarea
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-      ></textarea>
-      <div>
-        <button onClick={clickHandler}>Submit</button>
-      </div>
-      <pre>{code}</pre>
-      {/* iframe sandbox property set to an empty string blocks access even from the same origin (parent <=> child) */}
-      {/* iframe with sandbox property omitted or set to 'allow-same-origin' allows communitcation between child and parent */}
-      <iframe
-        ref={iframe}
-        title="code compiler window"
-        sandbox="allow-scripts"
-        srcDoc={html}
-      />
+      <CodeCell />
+      <CodeCell/>
     </div>
   );
 };
