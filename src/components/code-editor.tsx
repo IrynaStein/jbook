3e2f24adc3dc6df1902af319d1585d1d15a0ef7e
Iryna Stein <iryna@sensibleweather.com>
2022-12-12 18:21:58 -0700

    editor window

---
+++ src/components/code-editor.tsx
@@ -0,0 +1,68 @@
+import { useRef } from 'react';
+import MonacoEditor, { EditorDidMount } from '@monaco-editor/react';
+import prettier from 'prettier';
+import parser from 'prettier/parser-babel';
+import './code-editor.css';
+import './syntax.css'
+
+interface CodeEditorProps {
+  initialValue: string;
+  onChange(value: string): void;
+}
+const CodeEditor: React.FC<CodeEditorProps> = ({ onChange, initialValue }) => {
+  const editorRef = useRef<any>();
+
+  const onFormatClick = () => {
+    //get current value from editor
+    const unformatted = editorRef.current.getModel().getValue();
+    //format that value
+    const formatted = prettier
+      .format(unformatted, {
+        parser: 'babel',
+        plugins: [parser],
+        useTabs: false,
+        semi: true,
+        singleQuote: true,
+      })
+      .replace(/\n$/, '');
+    //set the formatter value back in the editor
+    editorRef.current.setValue(formatted);
+  };
+
+  const onEditorDidMount: EditorDidMount = (getValue, monacoEditor) => {
+    editorRef.current = monacoEditor;
+    monacoEditor.onDidChangeModelContent(() => {
+      onChange(getValue());
+    });
+    monacoEditor.getModel()?.updateOptions({ tabSize: 2 });
+  };
+  return (
+    <div className="editor-wrapper">
+      <button
+        className="button button-format is-primary is-small"
+        onClick={onFormatClick}
+      >
+        Format
+      </button>
+      <MonacoEditor
+        value={initialValue}
+        editorDidMount={onEditorDidMount}
+        height="200px"
+        language="javascript"
+        theme="dark"
+        options={{
+          wordWrap: 'on',
+          minimap: { enabled: false },
+          showUnused: false,
+          folding: false,
+          lineNumbersMinChars: 3,
+          fontSize: 16,
+          scrollBeyondLastLine: false,
+          automaticLayout: true,
+        }}
+      />
+    </div>
+  );
+};
+
+export default CodeEditor;
