9f197a158cfa20a05a1188da1474f77782c96e05
Iryna Stein <iryna@sensibleweather.com>
2022-12-16 09:30:04 -0700

    renamed code cell and added timer amd cleanup

---
+++ src/components/code-cell.tsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react';
+import Resizable from './resizable';
+import CodeEditor from './code-editor';
+import Preview from './preview';
+import bundle from '../bundler';
+import { cleanup } from '@testing-library/react';
+
+const CodeCell = () => {
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState<string>('');
+
+  useEffect(() => {
+    const timer = setTimeout(async () => {
+      const output = await bundle(input);
+      setCode(output);
+    }, 1000);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [input]);
+
+  return (
+    <Resizable direction="vertical">
+      <div style={{ height: '100%', display: 'flex', flexDirection: 'row' }}>
+        <Resizable direction="horizontal">
+          <CodeEditor
+            initialValue={'//happy hacking...'}
+            onChange={(value) => setInput(value)}
+          />
+        </Resizable>
+        {/* iframe sandbox property set to an empty string blocks access even from the same origin (parent <=> child) */}
+        {/* iframe with sandbox property omitted or set to 'allow-same-origin' allows communitcation between child and parent */}
+        <Preview code={code} />
+      </div>
+    </Resizable>
+  );
+};
+
+export default CodeCell;
