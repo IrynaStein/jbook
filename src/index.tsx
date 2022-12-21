f43b02da2a1cc4e7c1cf9829cf8cda6538cfda3e
Iryna Stein <iryna@sensibleweather.com>
2022-12-21 14:04:42 -0700

    commented out unused code

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
-import CodeCell from './components/code-cell';
+// import CodeCell from './components/code-cell';
+import TextEditor from './components/text-editor';
 
 const App = () => {
   return (
     <div>
-      <CodeCell />
+      {/* <CodeCell /> */}
       {/* <CodeCell/> */}
+      <TextEditor />
     </div>
   );
 };
