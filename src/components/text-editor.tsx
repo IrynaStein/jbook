5f809d4916c2acfea0a4bf6d981afdc26ee4afcb
Iryna Stein <iryna@sensibleweather.com>
2022-12-21 14:02:19 -0700

    text editor logic:

---
+++ src/components/text-editor.tsx
@@ -0,0 +1,50 @@
+import { useState, useEffect, useRef } from 'react';
+import MDEditor from '@uiw/react-md-editor';
+import './text-editor.css';
+
+const TextEditor: React.FC = () => {
+  const [editing, setEditing] = useState(false);
+  const [value, setValue] = useState('# Header');
+
+  const ref = useRef<HTMLDivElement | null>(null);
+  useEffect(() => {
+    const listener = (event: MouseEvent) => {
+      console.log(event.target);
+      if (
+        ref.current &&
+        event.target &&
+        ref.current.contains(event.target as Node)
+      ) {
+        console.log('element clicked on is inside editor');
+        return;
+      }
+      console.log('element is not inside editor');
+      setEditing(false);
+    };
+    document.addEventListener('click', listener, { capture: true });
+    return () => {
+      document.removeEventListener('click', listener, { capture: true });
+    };
+  }, []);
+  if (editing) {
+    return (
+      <div className="text-editor" ref={ref}>
+        <MDEditor
+          value={value}
+          onChange={(v) => {
+            setValue(v || '');
+          }}
+        />
+      </div>
+    );
+  }
+  return (
+    <div className="text-editor card" onClick={() => setEditing(true)}>
+      <div className="card-content">
+        <MDEditor.Markdown source={value} />
+      </div>
+    </div>
+  );
+};
+
+export default TextEditor;
