234d8901d2ccf046c4be2be09569d31969777c56
Iryna Stein <iryna@sensibleweather.com>
2022-12-21 17:57:22 -0700

    redux setup

---
+++ src/state/reducers/index.ts
@@ -0,0 +1,8 @@
+import cellsReducer from './cellsReducer';
+import { combineReducers } from 'redux';
+
+const reducers = combineReducers({ cells: cellsReducer });
+
+export default reducers;
+
+export type RootState = ReturnType<typeof reducers>;
