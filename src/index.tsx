0a6efcecfba425a57cd703c3a6fb4abe5b0ff993
Iryna Stein <iryna@sensibleweather.com>
2022-12-22 11:00:54 -0700

    added provider and store

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 // import CodeCell from './components/code-cell';
+import { Provider } from 'react-redux';
+import { store } from './state';
 import TextEditor from './components/text-editor';
 
 const App = () => {
   return (
-    <div>
-      {/* <CodeCell /> */}
-      {/* <CodeCell/> */}
-      <TextEditor />
-    </div>
+    <Provider store={store}>
+      <div>
+        {/* <CodeCell /> */}
+        {/* <CodeCell/> */}
+        <TextEditor />
+      </div>
+    </Provider>
   );
 };
 
